a32f01676dab061c4131d8e7c597540a42e1709a
abhay-zel <abhay.pratap@zel.finance>
2024-02-28 12:42:15 +0530

    validation

--- components/CreateNote.tsx
+++ components/CreateNote.tsx
@@ -4,6 +4,9 @@ import { useNoteStore } from '@/store/NoteStore';
 import Image from 'next/image';
 import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const colors = [
     "rgb(252 165 165)",
@@ -16,15 +19,31 @@ const colors = [
     
 const CreateNote = () => {
 
+    const notify = () => toast("Note Created Sucesfully!");
+
     const router = useRouter();
 
     const [imagePreview, setImagePreview] = useState<string | null>(null);
     const [colour, setColour] = useState<string>('rgb(252 165 165)');
     const [title, setTitle] = useState<string>('');
     const [content, setContent] = useState<string>('');
+    const [isValid, setIsValid] = useState<boolean>(false);
 
     const [notes, addNote] = useNoteStore((state) => 
         [state.notes, state.addNote]);
+
+    const handleTextChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        if(e.target.name==='title') {
+            setTitle(e.target.value);
+        }
+        else if(e.target.name==='content') {
+            setContent(e.target.value);
+        }
+
+        if(title.trim() !== '' && content.trim() !== ''){
+            setIsValid(true);
+        } 
+    }
 
     const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const file = e.target.files?.[0];
@@ -44,6 +63,11 @@ const CreateNote = () => {
     const handleSubmit = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         e.preventDefault();
 
+        if (!isValid) {
+            toast.error("Please enter a title and content!");
+            return;
+        }
+
         const note = {
             id: notes.length + 1,
             title,
@@ -53,6 +77,7 @@ const CreateNote = () => {
             timestamp: new Date(Date.now())
         }
         addNote(note);
+        notify();
         router.push('/');
         setTitle('');
         setContent('');
@@ -77,7 +102,7 @@ const CreateNote = () => {
             </div>
             <div className='mt-5'>
                 <label htmlFor='title' className='font-bold'>Title</label>
-                <input type='text' name='title' value={title} onChange={(e) => setTitle(e.target.value)} id='title' placeholder='Enter a Title' className='w-full border h-10 border-[#DBDBDB] outline-none p-3 pl-2.5' />
+                <input type='text' name='title' value={title} onChange={handleTextChange} id='title' placeholder='Enter a Title' className='w-full border h-10 border-[#DBDBDB] outline-none p-3 pl-2.5' />
             </div>
             <div className='mt-5 flex w-2/3'>
                 <div className='w-full'>
@@ -96,7 +121,7 @@ const CreateNote = () => {
             </div>
             <div className='mt-5'>
                 <label htmlFor='content' className='font-bold'>Content</label>
-                <textarea id='content' name='content' value={content} onChange={(e) => setContent(e.target.value)} placeholder='Enter Content' className='w-full border h-20 border-[#DBDBDB] outline-none p-3 pl-2.5' />
+                <textarea id='content' name='content' value={content} onChange={handleTextChange} placeholder='Enter Content' className='w-full border h-20 border-[#DBDBDB] outline-none p-3 pl-2.5' />
             </div>
             <div className='mt-5 flex flex-col'>
                 <label htmlFor='colour' className='font-bold'>Choose Colour</label>
@@ -109,6 +134,7 @@ const CreateNote = () => {
             </div>
             <div className='mt-10'>
                 <button className='bg-[#0055d1] text-white w-full h-11 rounded-md font-bold' onClick={(e) => handleSubmit(e)}>Create Note</button>
+                <ToastContainer theme='dark' />
             </div>
         </div>
       </div>
