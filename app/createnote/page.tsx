073feaf34d1130fead371ba94956ba1302f788e4
abhay-zel <abhay.pratap@zel.finance>
2024-02-29 21:07:46 +0530

    createnote folder

---
+++ app/createnote/page.tsx
@@ -0,0 +1,12 @@
+import CreateNote from '@/components/CreateNote'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div> 
+      <CreateNote />
+    </div>
+  )
+}
+
+export default page
